13d63a431d732ec4bcdbca57134f85b8c9d16dfd
Sanpi <sanpi@homecomputing.fr>
2020-04-15 15:50:40 +0200

    Adds pk! macro

--- romm/src/lib.rs
+++ romm/src/lib.rs
@@ -17,6 +17,37 @@ pub use row_structure::*;
 
 use std::collections::HashMap;
 
+/**
+ * Easily create pk argument for where clause, including find_by_pk function
+ *
+ * pk!(uuid)
+ * pk!(&uuid)
+ * pk!{ uuid, name }
+ * pk!{ "uuid" => uuid, "name" => name }
+ */
+#[macro_export]
+macro_rules! pk {
+    ($pk:ident) => {
+        $crate::pk!($pk,)
+    };
+
+    ($($pk:ident,)+) => {
+        $crate::pk!($(
+            $pk => $pk,
+        )*)
+    };
+
+    ($($key:expr => $value:expr,)+) => {{
+        let mut hash = std::collections::HashMap::new();
+
+        $(
+            hash.insert(stringify!($key), &$value as &dyn postgres::types::ToSql);
+        )*
+
+        hash
+    }}
+}
+
 pub struct Romm
 {
     default: String,
@@ -67,5 +98,51 @@ impl Romm
     pub fn get(&self, name: &str) -> Option<&Connection>
     {
         self.connections.get(&name.to_string())
+    }
+}
+
+impl Default for Romm
+{
+    fn default() -> Self
+    {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_pk_one() {
+        let uuid = "1234";
+        let pk = crate::pk!(uuid);
+
+        assert_eq!(pk.len(), 1);
+        assert!(pk.contains_key("uuid"));
+    }
+
+    #[test]
+    fn test_pk_multi() {
+        let uuid = "1234";
+        let name = "name";
+        let pk = crate::pk! {
+            uuid,
+            name,
+        };
+
+        assert_eq!(pk.len(), 2);
+        assert!(pk.contains_key("uuid"));
+        assert!(pk.contains_key("name"));
+    }
+
+    #[test]
+    fn test_pk_hash() {
+        let pk = crate::pk! {
+            uuid => "1234",
+            name => "name",
+        };
+
+        assert_eq!(pk.len(), 2);
+        assert!(pk.contains_key("uuid"));
+        assert!(pk.contains_key("name"));
     }
 }
