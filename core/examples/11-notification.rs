014cb5612f02272fac767f49405f01bb4cb8a252
Sanpi <sanpi@homecomputing.fr>
2020-12-03 16:34:43 +0100

    Adds nofication example

---
+++ core/examples/11-notification.rs
@@ -0,0 +1,24 @@
+const CHANNEL_NAME: &str = "channel_name";
+
+fn main() -> elephantry::Result<()> {
+    let database_url = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://localhost".to_string());
+    let elephantry = elephantry::Pool::new(&database_url)?;
+    elephantry.execute(include_str!("structure.sql"))?;
+
+    elephantry.listen(CHANNEL_NAME)?;
+    elephantry.notify(CHANNEL_NAME, Some("payload"))?;
+    listen(&elephantry);
+
+    elephantry.unlisten(CHANNEL_NAME)?;
+    elephantry.notify(CHANNEL_NAME, Some("payload"))?;
+    listen(&elephantry);
+
+    Ok(())
+}
+
+fn listen(elephantry: &elephantry::Connection) {
+    while let Some(notify) = elephantry.notifies() {
+        dbg!(notify);
+    }
+}
