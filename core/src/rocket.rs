64f8919c296b979226cc268c51aaffd0b077a7de
Sanpi <sanpi@homecomputing.fr>
2021-06-09 21:25:00 +0200

    Updates rocket to 0.5

--- core/src/rocket.rs
+++ core/src/rocket.rs
@@ -1,15 +1,19 @@
-use rocket_contrib::databases::{r2d2, DatabaseConfig, Poolable};
-
 #[cfg_attr(docsrs, doc(cfg(feature = "rocket")))]
-impl Poolable for crate::Connection {
+impl rocket_sync_db_pools::Poolable for crate::Connection {
     type Error = r2d2::Error;
     type Manager = crate::r2d2::ConnectionManager;
 
-    fn pool(config: DatabaseConfig<'_>) -> Result<r2d2::Pool<Self::Manager>, Self::Error> {
-        let manager = Self::Manager::new(config.url);
+    fn pool(
+        db_name: &str,
+        rocket: &rocket_sync_db_pools::rocket::Rocket<rocket_sync_db_pools::rocket::Build>,
+    ) -> rocket_sync_db_pools::PoolResult<Self> {
+        let config = rocket_sync_db_pools::Config::from(db_name, rocket)?;
+        let manager = Self::Manager::new(&config.url);
 
-        r2d2::Pool::builder()
+        let connection = r2d2::Pool::builder()
             .max_size(config.pool_size)
-            .build(manager)
+            .build(manager)?;
+
+        Ok(connection)
     }
 }
