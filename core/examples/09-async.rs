805b682484bc5ad05c90101ad3f9225cb6b935a4
Sanpi <sanpi@homecomputing.fr>
2025-08-24 11:36:38 +0200

    Uses smol instead of async_std

--- core/examples/09-async.rs
+++ core/examples/09-async.rs
@@ -11,32 +11,33 @@ mod employee {
     }
 }
 
-#[async_std::main]
-async fn main() -> elephantry::Result {
-    let database_url =
-        std::env::var("DATABASE_URL").unwrap_or_else(|_| "postgres://localhost".to_string());
-    let elephantry = elephantry::Pool::new(&database_url)?;
+fn main() -> elephantry::Result {
+    smol::block_on(async {
+        let database_url =
+            std::env::var("DATABASE_URL").unwrap_or_else(|_| "postgres://localhost".to_string());
+        let elephantry = elephantry::Pool::new(&database_url)?;
 
-    elephantry
-        .r#async()
-        .execute(include_str!("structure.sql"))
-        .await?;
+        elephantry
+            .r#async()
+            .execute(include_str!("structure.sql"))
+            .await?;
 
-    let employees = elephantry
-        .r#async()
-        .query::<employee::Entity>("select * from employee", &[])
-        .await?;
+        let employees = elephantry
+            .r#async()
+            .query::<employee::Entity>("select * from employee", &[])
+            .await?;
 
-    for employee in employees {
-        dbg!(employee);
-    }
+        for employee in employees {
+            dbg!(employee);
+        }
 
-    let total_salary = elephantry
-        .r#async()
-        .query_one::<bigdecimal::BigDecimal>("select sum(day_salary) from employee", &[])
-        .await?;
+        let total_salary = elephantry
+            .r#async()
+            .query_one::<bigdecimal::BigDecimal>("select sum(day_salary) from employee", &[])
+            .await?;
 
-    dbg!(total_salary);
+        dbg!(total_salary);
 
-    Ok(())
+        Ok(())
+    })
 }
